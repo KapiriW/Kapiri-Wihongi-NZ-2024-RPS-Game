8048f952cf139bb5e5e8f444c57794b3575d0f6d
KapiriW <143568240+KapiriW@users.noreply.github.com>
2024-05-01 14:23:32 +1200

    i forgot to commit sorry

---
+++ B_02_HlGame.py
@@ -0,0 +1,141 @@
+import math
+
+
+# checks users enter yes (y) or no (n)
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+        # checks user response, question
+        # repeats if users don't enter yes / no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter yes / no")
+def instruction():
+    print('''
+**** Instructions ****
+To begin, choose the number of rounds and either customise 
+the game parameters or go with the default game (where the 
+secret number will be between 1 and 100).
+Then choose how many rounds you'd like to play <enter> for 
+infinite mode.
+Your goal is to try to guess the secret number without 
+running out of guesses.
+ Good luck.   
+    ''')
+
+
+# checks for an integer more than 0 (allows <enter>)
+def int_check(question):
+    while True:
+        error = "Please enter an integer that is 1 or more."
+# checks for an integer with optional upper /
+# lower limits and an optional exit code for infinite mode
+# / quitting the game
+def int_check(question, low=None, high=None, exit_code=None):
+    # if any integer is allowed...
+    if low is None and high is None:
+        error = "Please enter an integer"
+
+    # if the number needs to be more than an
+    # integer (ie: rounds / 'high number')
+    elif low is not None and high is None:
+        error = (f"Please enter an integer that is "
+                 f"more than / equal to {low}")
+
+        to_check = input(question)
+    # if the number needs to between low & high
+    else:
+        error = (f"Please enter an integer that"
+                 f" is between {low} and {high} (inclusive)")
+
+    while True:
+        response = input(question).lower()
+
+        # check for infinite mode
+        if to_check == "":
+            return "infinite"
+        # check for infinite mode / exit code
+        if response == exit_code:
+            return response
+
+        try:
+            response = int(to_check)
+            response = int(response)
+
+            # Check the integer is not too low...
+            if low is not None and response < low:
+                print(error)
+
+            # checks that the number is more than / equal to 1
+            if response < 1:
+             #check response is more than the low number
+            elif high is not None and response > high:
+                print(error)
+
+            # if the response is valid, return it
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# calculate the maximum number of guesses
+def calc_guesses(low, high):
+    num_range = high - low + 1
+    max_raw = math.log2(num_range)
+    max_upped = math.ceil(max_raw)
+    max_guesses = max_upped + 1
+    return max_guesses
+
+
+# Main Routine Starts here
+
+# Intialise game variables
+mode = "regular"
+rounds_played = 0
+print("🔼🔼🔼 Welcome to the Higher Lower Game 🔻🔻🔻")
+print()
+want_instructions = yes_no("Do you want to read the instructions? ")
+# checks users enter yes (y) or no (n)
+if want_instructions == "yes":
+    instruction()
+
+# Ask user for number of rounds / infinite mode
+num_rounds = int_check("How many rounds would you like? Push <enter> for infinite mode: ")
+num_rounds = int_check("Rounds <enter for infinite>: ",
+                       low=1, exit_code="")
+
+if num_rounds == "infinite":
+    mode = "infinite"
+    num_rounds = 5
+
+# Get Game parameters
+low_num = int_check("Low Number? ")
+high_num = int_check("High Number? ", low=low_num + 1)
+guesses_allowed = calc_guesses(low_num, high_num)
+
+# Game loop starts here
+while rounds_played < num_rounds:
+
+    # Rounds headings (based on mode)
+    if mode == "infinite":
+        rounds_heading = f"\n♾♾♾ Round {rounds_played + 1} (Infinite Mode) ♾♾♾"
+    else:
+        rounds_heading = f"\n💿💿💿 Round {rounds_played + 1} of {num_rounds} 💿💿💿"
+    print(rounds_heading)
+    print()
+    # get user choice
+    user_choice = input("Choose: ")
+    # If user choice is the exit code, break the loop
+    if user_choice == "xxx":
+        break
+    rounds_played += 1
+    # if users are in infinite mode, increase number of rounds!
+    if mode == "infinite":
+        num_rounds += 1
+# Game loop ends here
+# Game History / Statistics area
---
+++ c_02.py
@@ -0,0 +1,25 @@
+# checks for valid integers (with optional low value,
+# high value and exit code)
+def int_check(question, low=None, high=None, exit_code=None):
+            print(error)
+
+
+# rounds = "test"
+# while rounds != "":
+#     rounds = int_check("Rounds <enter for infinite>: ", low=1, exit_code="")
+#     print(f"You asked for {rounds}")
+#
+# print("You chose infinite mode")
+
+# low_num = int_check("Low Number? ")
+# print(f"You chose a low number of {low_num}")
+
+# high_num = int_check("High Number? ", low=1)
+# print(f"You chose a high number of {high_num}")
+
+# Check user guesses
+# guess = ""
+# while guess != "xxx":
+#     guess = int_check("Guess: ", low=0, high=10, exit_code="xxx")
+#     print(f"You guessed {guess}")
+#     print()
